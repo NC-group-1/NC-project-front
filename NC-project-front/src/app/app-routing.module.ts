b27c1294da074422eaa4b123af6ce9b253e53081
Demenkova <lera145dem@gmail.com>
2020-11-22 17:20:54 +0200

    local commit

--- NC-project-front/src/app/app-routing.module.ts
+++ NC-project-front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CreateUserComponent} from './create-user/create-user.component';
+import {ListUsersComponent} from './list-users/list-users.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'createUser', component: CreateUserComponent},
+  {path: 'listUsers', component: ListUsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- NC-project-front/src/app/app.module.ts
+++ NC-project-front/src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateUserComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatRadioModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
