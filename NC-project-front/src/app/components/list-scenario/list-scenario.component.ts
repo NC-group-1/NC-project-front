25b0f745fc9935ec6065d7abedf3ac11d23b6df0
Demenkova <lera145dem@gmail.com>
2020-12-03 15:19:48 +0200

    Some change

---
+++ NC-project-front/src/app/components/list-scenario/list-scenario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-scenario',
+  templateUrl: './list-scenario.component.html',
+  styleUrls: ['./list-scenario.component.css']
+})
+export class ListScenarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
