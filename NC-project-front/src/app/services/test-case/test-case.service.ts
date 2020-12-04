c4379f9ec76085366359fc51ad353d782d904d04
Ivan Ivanishyn <i.v.ivanishyn@gmail.com>
2020-12-04 15:12:35 +0200

    fixed/apiPath

--- NC-project-front/src/app/services/test-case/test-case.service.ts
+++ NC-project-front/src/app/services/test-case/test-case.service.ts
@@ -5,38 +5,39 @@ import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
 import {TestCaseResponseModel} from '../../../models/TestCaseResponseModel';
 import {tap} from 'rxjs/operators';
+import {apiPath} from '../../../../globals'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestCaseService {
 
-  private urlPath = 'http://localhost:8081/';
-
   constructor(private httpClient: HttpClient, private router: Router) {}
 
   getPaginatedTestCases(pageSize: number, pageIndex: number, filter: string, orderBy: string, order: string): Observable<TestCaseResponseModel>{
     return this.httpClient.get<TestCaseResponseModel>(
-      this.urlPath + 'api/test_case/get_test_case_list/' + pageIndex + '/' + pageSize
-      + '?filter=' + filter
+      apiPath + 'api/test_case/list'
+      + '?pageSize=' + pageSize
+      + '&pageIndex=' + pageIndex
+      + '&filter=' + filter
       + '&orderBy=' + orderBy
-      + '&order=' + order
-    ).pipe(tap(() => {}, e => {if (e.status) { this.router.navigate(['404']); } }));
+      + '&order=' + order)
+      .pipe(tap(() => {}, e => {if (e.status) { this.router.navigate(['404']); } }));
   }
 
   updateTestCase(testCase: TestCaseModel): Observable<any> {
     const httpOption = {
       headers:new HttpHeaders({"Content-Type":"application/json"})
     }
-    return this.httpClient.post(this.urlPath + 'api/test_case/update', JSON.stringify(testCase),httpOption);
+    return this.httpClient.post(apiPath + 'api/test_case/update', JSON.stringify(testCase),httpOption);
   }
 
   deleteTestCase(id: number) {
-    return this.httpClient.delete(this.urlPath + 'api/test_case/' + id);
+    return this.httpClient.delete(apiPath + 'api/test_case/' + id);
   }
 
   /*postTestCase(testCase: TestCaseModel) {
-    return this.httpClient.post(this.urlPath + 'test_case', testCase);
+    return this.httpClient.post(this.apiPath + 'test_case', testCase);
   }*/
 
 }
