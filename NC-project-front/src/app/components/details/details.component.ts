98dfb4fbc4a387750e015017f8a1094824180b73
Slava Demianenko <slavadem100500@gmail.com>
2020-12-21 18:47:20 +0200

    progress bar and actions table working

--- NC-project-front/src/app/components/details/details.component.ts
+++ NC-project-front/src/app/components/details/details.component.ts
@@ -42,7 +42,7 @@ export class DetailsComponent implements OnInit {
   //   {id: 8, name: 'Action 4', dataset: 'Dataset 7', result: 'Result 3', status: 'failed'}];
   testCase: DetailsTestCaseModel;
   size: number;
-  dataSource: any;
+  dataSource = new MatTableDataSource<ActionInstRun>([]);
   columnToDisplay = ['actionName', 'dataSetName', 'status'];
   expandedElement: ActionInstRun[] | null;
   length: number;
@@ -50,7 +50,7 @@ export class DetailsComponent implements OnInit {
   username: string;
   detailsProgress: ActionInstRun[];
   id: number;
-  tcProgress: TestCaseProgressModel[] = [];
+  tcProgress: TestCaseProgressModel;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
@@ -61,40 +61,58 @@ export class DetailsComponent implements OnInit {
               private webSocketService: WebSocketService,
               private tcService: TestCaseService) {
     this.id = parseInt(this.router.url.split('/')[3], 10);
-    this.dataSource = new MatTableDataSource();
+    this.dataSource.paginator = this.paginator;
     this.detailsProgress = [];
-    this.tcService.getTestCaseDetailsById(this.id)
-      .subscribe(
-        value => this.testCase = value
-      );
-    this.stompCase = this.webSocketService.connect();
-    this.stompCase.connect({}, () => {
-      this.tcService.getAllActionInstRun(this.id).subscribe(value => {
-        this.testCase.actionInstRunDtos = value;
-        console.log(this.testCase);
-        this.dataSource = new MatTableDataSource(value);
-        this.dataSource.paginator = this.paginator;
-        this.subscribeOnTcProgress(this.id);
-        this.stompCase.send('/app/actionInst/tc', {}, +this.id);
-        this.stompCase.subscribe('/topic/actionInst/' + this.id, response => {
-          this.detailsProgress.push(JSON.parse(response.body));
+  }
+
+  private reloadTestCaseDetails(id: number) {
+    this.tcService.getTestCaseDetailsById(id)
+      .subscribe(value => {
+        this.testCase = value;
+        this.stompCase = this.webSocketService.connect();
+        this.stompCase.connect({}, () => {
+          this.subscribeOnTcProgress(id);
+          if (value.status !== 'PASSED' && value.status !== 'FAILED' && value.status !== 'CANCELED') {
+            this.subscribeOnActionInstUpdates(id);
+          } else {
+            this.tcService.getAllActionInstRun(id).subscribe(actionInstRuns => {
+              this.testCase.actionInstRunDtos = actionInstRuns;
+              console.log(this.testCase);
+              // this.dataSource = new MatTableDataSource(actionInstRuns);
+              this.dataSource.data = actionInstRuns;
+            });
+          }
         });
       });
+  }
+
+  private subscribeOnActionInstUpdates(id: number) {
+    this.stompCase.send('/app/actionInst/tc', {}, +this.id);
+    this.stompCase.subscribe('/topic/actionInst/' + this.id, response => {
+      console.log(JSON.parse(response.body));
+      const data = this.dataSource.data;
+      JSON.parse(response.body).forEach(actionInstRun => {
+        data.push(actionInstRun);
+      });
+      this.dataSource.data = data;
     });
   }
 
   private subscribeOnTcProgress(id: number) {
     this.stompCase.send('/app/progress/tc', {}, id);
     this.stompCase.subscribe('/topic/progress/' + id, progress => {
-      console.log(progress);
-      this.tcProgress.push(JSON.parse(progress.body));
+      this.tcProgress = JSON.parse(progress.body);
+      if (this.tcProgress.progress === 1 && this.tcProgress.status === 'IN_PROGRESS') {
+        this.reloadTestCaseDetails(this.id);
+      }
     });
   }
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(() => {
-      this.dataSource = new MatTableDataSource(this.detailsProgress);
+      // this.dataSource = new MatTableDataSource(this.detailsProgress);
     });
+    this.reloadTestCaseDetails(this.id);
   }
 
 }
